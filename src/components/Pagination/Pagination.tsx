47e4a4971cdbfb586e4367026999ec12ce4263f1
Matija Turčec <turcec.matija@gmail.com>
2021-02-24 19:01:51 +0100

    fixed bugs, final version, hopefully

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -11,22 +11,22 @@ function Pagination( {currentPage, pageCount}: IPaginationProps ) {
   // -2 = "..."
   // -1 = ">"
   const conditionalButtons = [
-    <PButton label={1} />,
+    <PButton key={0} label={1} />,
 
-    currentPage>4 ? <PButton label={-2} /> : <PButton label={2} />,
+    currentPage>4 ? <PButton key={1} label={-2} /> : <PButton key={2} label={2} />,
 
-    currentPage<=4 ? <PButton label={3} /> :
-      currentPage>(pageCount-3) ? <PButton label={pageCount-4} /> : <PButton label={currentPage-1} />,
+    currentPage<=4 ? <PButton key={3} label={3} /> :
+      currentPage>(pageCount-3) ? <PButton key={4} label={pageCount-4} /> : <PButton key={5} label={currentPage-1} />,
     
-    currentPage<=4 ? <PButton label={4} /> :
-      currentPage>(pageCount-3) ? <PButton label={pageCount-3} /> : <PButton label={currentPage} />,
+    currentPage<=4 ? <PButton key={6} label={4} /> :
+      currentPage>(pageCount-3) ? <PButton key={7} label={pageCount-3} /> : <PButton key={8} label={currentPage} />,
     
-    currentPage<=4 ? <PButton label={5} /> :
-    currentPage>(pageCount-3) ? <PButton label={pageCount-2} /> : <PButton label={currentPage+1} />,
+    currentPage<=4 ? <PButton key={9} label={5} /> :
+    currentPage>(pageCount-3) ? <PButton key={10} label={pageCount-2} /> : <PButton key={11} label={currentPage+1} />,
     
-    currentPage<(pageCount-3) ? <PButton label={-2} /> : <PButton label={pageCount-1} />,
+    currentPage<(pageCount-3) ? <PButton key={12} label={-2} /> : <PButton key={13} label={pageCount-1} />,
 
-    <PButton label={pageCount} />
+    <PButton key={14} label={pageCount} />
   ]
   return (
     <>
