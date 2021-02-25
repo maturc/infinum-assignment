281b0d1c9596af8c732648d9331250468a11177f
Matija Turčec <turcec.matija@gmail.com>
2021-02-25 10:25:10 +0100

    added links to detail pages

--- src/components/PeopleTable/PeopleTableItem.tsx
+++ src/components/PeopleTable/PeopleTableItem.tsx
@@ -1,11 +1,14 @@
+import {Link} from "react-router-dom";
+
 type IPeopleTableItemProps = {
   person: any;
 }
 function PeopleTableItem( {person}: IPeopleTableItemProps ) {
+  const id = person.url.split("/")[5]
   return (
     <>
       <tr>
-        <td>{person.name}</td>
+        <td><Link to={`/details/${id}`}>{person.name}</Link></td>
         <td>{person.birth_year}</td>
       </tr>
     </>
