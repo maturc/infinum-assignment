4f8b8570bcdb4c531f067d774f3861b89b2acea9
Matija Turčec <turcec.matija@gmail.com>
2021-02-25 10:09:12 +0100

    new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Details from './routes/Details/Details';
 import Home from './routes/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+          <Route path="/details/:id" children={<Details />} />
           <Route path="/:page?/:search?" children={<Home />} />
           <Route path="/" children={<Home />} />
       </Switch>
